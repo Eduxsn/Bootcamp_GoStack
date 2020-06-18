6f61d0ff2f93b82c32566cad13642147f54e9523
Eduardo <eduneves95@gmail.com>
2020-06-17 22:18:01 -0300

    Minor changes

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+
+import './App.css';
+import backgroundImage from './assets/02.jpeg';
+
+import Header from './components/Header';
+
+
+function App() {
+    const [projects, setProjects] = useState(["Desenvolvimento de app", "Front-end web"]);
+
+    function handleAddProject() {
+        // projects.push(`Novo projeto ${Date.now()}` )
+
+        setProjects([...projects, `Novo projeto ${Date.now()}`]);
+
+    }
+
+    return (
+        <>
+            <Header title="Projects" /> 
+
+            <img width={300} src={backgroundImage} alt=""/>
+
+            <ul>
+                {projects.map(project => <li key={project}>{project}</li>)}
+            </ul>
+
+            <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
+        </>
+    );
+    
+}
+
+export default App;
